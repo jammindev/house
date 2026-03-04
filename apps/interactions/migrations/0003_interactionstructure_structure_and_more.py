3a89d3bd532d3491ca49a741637e3782604e8686
Ben Jammin <benjamin.vandamme@me.com>
2026-03-04 19:51:17 +0100

    fix: update foreign key reference to structure in interaction migrations and adjust contact indexes

--- apps/interactions/migrations/0003_interactionstructure_structure_and_more.py
+++ apps/interactions/migrations/0003_interactionstructure_structure_and_more.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='interactionstructure',
             name='structure',
-            field=models.ForeignKey(db_column='structure_id', on_delete=django.db.models.deletion.CASCADE, related_name='interaction_structures', to='structures.structure'),
+            field=models.ForeignKey(db_column='structure_id', on_delete=django.db.models.deletion.CASCADE, related_name='interaction_structures', to='directory.structure'),
         ),
         migrations.AddIndex(
             model_name='interactioncontact',
