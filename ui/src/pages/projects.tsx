be02db5eb2497f00da9a480e93208819d0866c37
Ben Jammin <benjamin.vandamme@me.com>
2026-03-07 21:41:00 +0100

    feat: add project-related components and mount logic for project detail, edit, group detail, group list, new project, and projects pages

---
+++ ui/src/pages/projects.tsx
@@ -0,0 +1,15 @@
+import type { ComponentProps } from 'react';
+
+import ProjectList from '../../../apps/projects/react/ProjectList';
+import { mountWithJsonScriptProps, onDomReady } from '@/lib/mount';
+
+type ProjectListProps = ComponentProps<typeof ProjectList>;
+
+onDomReady(() => {
+  mountWithJsonScriptProps<ProjectListProps>(
+    'projects-list-root',
+    'projects-list-props',
+    ProjectList,
+    { withToaster: true }
+  );
+});
